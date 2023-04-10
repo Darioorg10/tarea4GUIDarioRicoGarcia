371d95e77f048cdacdac943b62e7f7dd7f0b90a9
darioorg10 <dricgar1103@g.educaand.es>
2023-04-10 10:04:58 +0200

    + Creamos una clase calculadora en la que creamos un objeto tipo calculadora, así tenemos la funcionalidad en una clase y el main en otra

---
+++ src/main/java/ejercicioCompleto/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicioCompleto;
+
+/**
+ *
+ * @author dario
+ */
+public class Calculadora {
+    
+    public static void main(String[] args) {
+        
+        CalculadoraCompleta calculadora = new CalculadoraCompleta();
+        calculadora.setVisible(true);
+        
+    }
+    
+}
